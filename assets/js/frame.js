0a5f41b7ee95c53939c0c9ca743ba6f2c59c22f5
Laci556 <bucsailaca@gmail.com>
2019-05-06 00:03:11 +0200

    macOS bezárás dock-ra

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -1,45 +1,50 @@
+(function() {
+  const remote = require('electron').remote;
 
-(function () {
-  const remote = require('electron').remote
+  function init() {
+    document.getElementById('min-btn').addEventListener('click', function(e) {
+      const window = remote.getCurrentWindow();
+      window.minimize();
+    });
 
-  function init () {
-    document.getElementById('min-btn').addEventListener('click', function (e) {
-      const window = remote.getCurrentWindow()
-      window.minimize()
-    })
-
-    document.getElementById('max-btn').addEventListener('click', function (e) {
-      const window = remote.getCurrentWindow()
+    document.getElementById('max-btn').addEventListener('click', function(e) {
+      const window = remote.getCurrentWindow();
       if (!window.isMaximized()) {
-        window.maximize()
+        window.maximize();
       } else {
-        window.unmaximize()
+        window.unmaximize();
       }
-    })
+    });
 
-    document.getElementById('close-btn').addEventListener('click', function (e) {
-      const window = remote.getCurrentWindow()
-      const app = remote.app
-      require('./js/file').getGlobal("settings", {}).then(function(result){
-        var shouldClose = true;
-        if(result["totray"]){
-          shouldClose = false;
-        }
-        if(shouldClose){
-
-          console.log(shouldClose);
-          app.quit();
-        } else {
-          require('./js/tray').startTray(window);
-          window.hide();
-        }
-      });
-    })
-  };
-
-  document.onreadystatechange = function () {
-    if (document.readyState == 'complete') {
-      init()
-    }
+    document.getElementById('close-btn').addEventListener('click', function(e) {
+      const window = remote.getCurrentWindow();
+      const app = remote.app;
+      // macOS-en a bezárás gomb alapértelmezetten nem zárja be teljesen az alkalmazást, csak a dock-ra teszi
+      if (require('os').platform == 'darwin') {
+        window.close();
+      } else {
+        require('./js/file')
+          .getGlobal('settings', {})
+          .then(function(result) {
+            var shouldClose = true;
+            if (result['totray']) {
+              shouldClose = false;
+            }
+            if (shouldClose) {
+              console.log(shouldClose);
+              app.quit();
+            } else {
+              require('./js/tray').startTray(window);
+              window.hide();
+            }
+          });
+      }
+    });
   }
-})()
+
+  document.onreadystatechange = function() {
+    if (document.readyState == 'complete') {
+      init();
+    }
+  };
+})();
